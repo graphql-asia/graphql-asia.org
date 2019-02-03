037e2a673fd9805f4769a3c8604a43d51facabb1
Tobias Meixner <meixner.tobias@gmail.com>
2019-02-03 17:38:39 +0700

    FB link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => (
         <div className="inner">
             <ul className="icons">
                 <li><a href="https://twitter.com/graphqlasia" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>                
+                <li><a href="https://www.facebook.com/graphqlasia" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>                
                 <li><a href="https://www.linkedin.com/company/graphql-asia/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
             <ul className="copyright">
