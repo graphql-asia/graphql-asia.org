17ba5c0fda17202a368841f5fa309d229dfb6895
Tobias Meixner <meixner.tobias@gmail.com>
2019-03-19 01:51:19 +0700

    info icons alpha

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -3,7 +3,8 @@ import Img from "gatsby-image"
 
 class Speaker extends React.Component {
     state = {      
-      showDetail: false
+      showTalkDetail: false,
+      showSpeakerDetail: false
     };
     render() {
         const props = this.props;
@@ -47,24 +48,98 @@ class Speaker extends React.Component {
                         {props.time}
                     </span>
                     <header className="major">
-                        <h3>{props.speakerName}</h3>
+                        <h3>{props.speakerName}
+                        {
+                        props.speakerDescription ?
+                        <span style={{
+                            cursor: 'pointer',
+                            background: 
+                                this.state.showSpeakerDetail ?
+                                '#fff':'transparent',
+                            color: 
+                                this.state.showSpeakerDetail ?
+                                '#777':'#fff',
+                            
+                            borderRadius: '100%',
+                            height: '1em',
+                            width: '1em',
+                            border: '1px solid #fff',
+                            display: 'inline-block',                            
+                            verticalAlign: 'middle',
+                            lineHeight: '1em',
+                            fontSize: '0.5em',                            
+                            marginLeft: '.75em'                        
+                        }} onClick={()=>this.setState({showSpeakerDetail:!this.state.showSpeakerDetail})} className="infoIcon icon fa-info"></span>
+                        :null
+                        }
+                        </h3>                        
                     </header>                
-                    <span className="speakerPosition">{props.speakerPosition}</span>         
+                    {
+                        props.isAgenda === true && props.speakerDescription && this.state.showSpeakerDetail === true ?
+                        <div style={{
+                            fontSize: '1rem',
+                            position: 'absolute',
+                            background: '#525252',
+                            border: '2px solid white',
+                            padding: '1em',
+                            width: '400px',
+                            maxWidth: '100%',
+                            zIndex: 200
+                        }} className="speakerDetail">
+                            <span className="speakerDescription">{props.speakerDescription}</span>
+                        </div>
+                        :null
+                    }
+                    <span  className="speakerPosition">{props.speakerPosition}</span>                                        
                     <br/>
                     {
                         props.isAgenda === true ?
+                        <div>
                         <span className="talkTitle">{props.talkTitle}</span>
+                        {
+                            props.talkAbstract ?
+                            <span style={{
+                                cursor: 'pointer',
+                                background: 
+                                    this.state.showSpeakerDetail ?
+                                    '#fff':'transparent',
+                                color: 
+                                    this.state.showSpeakerDetail ?
+                                    '#777':'#fff',
+                                
+                                borderRadius: '100%',
+                                height: '1em',
+                                width: '1em',
+                                border: '1px solid #fff',
+                                display: 'inline-block',                            
+                                verticalAlign: 'middle',
+                                lineHeight: '1em',
+                                fontSize: '0.5em',                            
+                                marginLeft: '.75em'                        
+                            }} onClick={()=>this.setState({showTalkDetail:!this.state.showTalkDetail})} className="infoIcon icon fa-info"></span>
+                            :null
+                        }                        
+                        </div>
                         :
                         null                    
                     }
                     {
-                        props.isAgenda === true ?
-                        <span className="talkAbstract">{props.talkAbstract}</span>
+                        props.isAgenda === true && props.talkAbstract && this.state.showTalkDetail===true ?
+                        <span style={{
+                            fontSize: '1rem',
+                            position: 'absolute',
+                            background: '#525252',
+                            border: '2px solid white',
+                            padding: '1em',
+                            width: '400px',
+                            maxWidth: '100%',
+                            zIndex: 200
+                        }} className="talkAbstract">{props.talkAbstract}</span>
                         :
                         null                    
                     }
                     <br/>          
-                    {
+                    {/* {
                         props.isAgenda === true && props.speakerDescription ?
                             this.state.showDetail === true ?
                             <div className="speakerDetail">
@@ -82,7 +157,7 @@ class Speaker extends React.Component {
                                 fontSize: '80%'
                             }} onClick={()=>this.setState({showDetail:true})}>more...</span>
                         :null
-                    }                    
+                    }                     */}
                 </div>
             </div>
         </section>
